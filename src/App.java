0ef7ff4406deab8d4c2fafe07b7ba1814cdfa4d7
Daniel Aguilar <dyangeltk17@gmail.com>
2022-08-07 16:47:59 -0500

    initial commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class App {
+
+    // public static final String ANSI_RED = "\u001B[31m";
+    public static void main(String[] args) throws Exception {
+
+        Scanner sn = new Scanner(System.in);
+        String text;
+        String store = "";
+        char option;
+
+        do {
+            System.out.println("Ingrese el texto a concatenar:");
+            text = sn.next();
+            store += text;
+            System.out.println("Mostrando texto concatenado: " + store);
+            System.out.println("-----------------------------------");
+            System.out.println("¿Quiere seguir concatenando?. Escriba Y/N");
+            option = sn.next().charAt(0);
+            // for (int i = 0; i < args.length; i++) {
+            // }
+        } while (option == 'y' || option == 'Y');
+    }
+}
