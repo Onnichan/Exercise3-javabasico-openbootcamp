78ee37d1ebf032ec4659e96c9974a89d4567103d
Daniel Aguilar <dyangeltk17@gmail.com>
2022-08-07 17:18:04 -0500

    initial commit

--- src/App.java
+++ src/App.java
@@ -1,19 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
 
-    // public static final String ANSI_RED = "\u001B[31m";
+
+    /*
+     * En este ejercicio tenéis que crear un bucle que permita concatenar textos e imprima el resultado final por consola.
+     * Tened en cuenta que los textos pueden venir de un array de tipo String. Por ejemplo:
+     */
     public static void main(String[] args) throws Exception {
 
         Scanner sn = new Scanner(System.in);
         String text;
-        String store = "";
         char option;
-
+        
+        List<String> listas = new ArrayList<>();
+        
         do {
             System.out.println("Ingrese el texto a concatenar:");
             text = sn.next();
-            store += text;
+            listas.add(text);
+            String store = "";
+            for (String texto : listas) {
+                store += texto;
+            }
             System.out.println("Mostrando texto concatenado: " + store);
             System.out.println("-----------------------------------");
             System.out.println("¿Quiere seguir concatenando?. Escriba Y/N");
